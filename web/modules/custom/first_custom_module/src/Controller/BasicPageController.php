969ae97ae714f1c5dc25e601c8b2ce317c564ac4
Rohan Singh <rohan.singh@innoraft.com>
2023-06-27 15:55:44 +0530

    Pushed the custom module folder

---
+++ web/modules/custom/first_custom_module/src/Controller/BasicPageController.php
@@ -0,0 +1,24 @@
+<?php
+   namespace Drupal\first_custom_module\Controller;
+
+   use Drupal\Core\Controller\ControllerBase;
+
+   class BasicPageController extends ControllerBase {
+    
+    public function Page1(){
+        $user = \Drupal::currentUser()->getAccountName();
+        return [
+            '#type'=> 'markup',
+            '#markup'=> $this->t('Hi '.$user.'!!')
+        ];
+    }
+
+    public function Page2(){
+        return [
+            '#title'=> 'Hi arijit this side',
+            '#data'=> 'This is the data field'
+        ];
+    }
+
+   }
+?>
