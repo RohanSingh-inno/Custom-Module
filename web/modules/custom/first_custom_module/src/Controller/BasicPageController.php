15b333a486d3809ab3b3944f4b218eb1a605bfcc
Rohan Singh <rohan.singh@innoraft.com>
2023-07-05 12:04:02 +0530

    FT2023-339: Made changes according to PR review of peer's repo by mentor.

--- web/modules/custom/first_custom_module/src/Controller/BasicPageController.php
+++ web/modules/custom/first_custom_module/src/Controller/BasicPageController.php
@@ -1,24 +1,43 @@
 <?php
-   namespace Drupal\first_custom_module\Controller;
+namespace Drupal\first_custom_module\Controller;
 
-   use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Controller\ControllerBase;
 
-   class BasicPageController extends ControllerBase {
-    
-    public function Page1(){
+/**
+ * Class for showing the pages.
+ */
+class BasicPageController extends ControllerBase
+{
+
+    /**
+     * Display the markup.
+     *
+     * @return array
+     *   Return markup array.
+     */
+    public function Page1()
+    {
         $user = \Drupal::currentUser()->getAccountName();
         return [
-            '#type'=> 'markup',
-            '#markup'=> $this->t('Hi '.$user.'!!')
+            '#type' => 'markup',
+            '#markup' => $this->t('Hi ' . $user . '!!')
         ];
     }
 
-    public function Page2(){
+    /**
+     * Display the markup.
+     *
+     * @return array
+     *   Return markup array.
+     */
+    public function Page2()
+    {
         return [
-            '#title'=> 'Hi arijit this side',
-            '#data'=> 'This is the data field'
+            '#title' => 'Hi arijit this side',
+            '#data' => 'This is the data field'
         ];
     }
 
-   }
+}
+
 ?>
