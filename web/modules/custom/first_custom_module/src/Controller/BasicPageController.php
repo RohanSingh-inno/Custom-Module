e24691b8defb0dce9153e187a85f82cb40c14de3
Rohan Singh <rohan.singh@innoraft.com>
2023-07-05 11:58:20 +0530

    FT2023-340: Made Changes according to the mentor's review on peer's repo

--- web/modules/custom/first_custom_module/src/Controller/BasicPageController.php
+++ web/modules/custom/first_custom_module/src/Controller/BasicPageController.php
@@ -1,34 +1,43 @@
 <?php
-   namespace Drupal\first_custom_module\Controller;
+namespace Drupal\first_custom_module\Controller;
 
-   use Drupal\Core\Controller\ControllerBase;
-//    use Drupal\Core\Cache\CacheableMetadata;
-//    use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Controller\ControllerBase;
 
-   class BasicPageController extends ControllerBase {
-    
-    // public function __construct(EntityFieldManagerInterface $entityFieldManager) {
-    //     $this->entityFieldManager = $entityFieldManager;
-    //   }
-    public function Page1(){
-        // $cacheTags = $this->entityFieldManager->getDefinition('user')->getListCacheTags();
-        // $cacheability = (new CacheableMetadata())->setCacheTags($cacheTags);
-        // $userName = \Drupal::currentUser()->getAccountName();
-      
-        // $cacheability->applyTo($userName);
-        $user = \Drupal::currentUser()->getAccountName();
-        return [
-            '#type'=> 'markup',
-            '#markup'=> $this->t('Hi '.$user.'!!')
-        ];
-    }
+/**
+ * Class for showing the pages
+ */
+class BasicPageController extends ControllerBase
+{
 
-    public function Page2(){
-        return [
-            '#title'=> 'Hi arijit this side',
-            '#data'=> 'This is the data field'
-        ];
-    }
+  /**
+   * Display the markup.
+   *
+   * @return array
+   *   Return markup array.
+   */
+  public function Page1()
+  {
+    $user = \Drupal::currentUser()->getAccountName();
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('Hi ' . $user . '!!')
+    ];
+  }
 
-   }
+  /**
+   * Display the markup.
+   *
+   * @return array
+   *   Return markup array.
+   */
+  public function Page2()
+  {
+    return [
+      '#title' => 'Hi arijit this side',
+      '#data' => 'This is the data field'
+    ];
+  }
+
+}
+
 ?>
