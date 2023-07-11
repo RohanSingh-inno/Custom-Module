5bfb6fd11cb91527ecafe6d43a4c51a75619dffa
Rohan Singh <rohan.singh@innoraft.com>
2023-07-11 12:10:59 +0530

    Processed code through Php Code Sniffer

--- web/modules/custom/first_custom_module/src/Controller/BasicPageController.php
+++ web/modules/custom/first_custom_module/src/Controller/BasicPageController.php
@@ -2,53 +2,65 @@
 
 namespace Drupal\first_custom_module\Controller;
 
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class for showing the pages.
  */
-class BasicPageController extends ControllerBase{
+class BasicPageController extends ControllerBase {
 
-    protected $account;
+  /**
+   * Account information.
+   *
+   * @var string
+   */
+  protected $account;
 
-    public function __construct(AccountInterface $account){
-      $this->account = $account;
-    }
+  /**
+   * Constructor function.
+   */
+  public function __construct(AccountInterface $account) {
+    $this->account = $account;
+  }
 
-    public static function create(ContainerInterface $container){
-      return new static(
-        $container->get('current_user')
-      );
-    }
+  /**
+   * Function to get current user.
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('current_user')
+    );
+  }
 
-    /**
-     * Display the markup.
-     *
-     * @return array
-     *   Return markup array.
-     */
-    public function page1(){
-      return [
+  /**
+   * Display the markup.
+   *
+   * @return array
+   *   Return markup array.
+   */
+  public function page1() {
+    $name = $this->account->getAccountName();
+    return [
       '#type' => 'markup',
-      '#markup' => $this->t('Hi ' . $this->account->getAccountName() . '!!')
-      ];
-    }
+      '#markup' => $this->t('Hi @name !!', [
+        "@name" => $name,
+      ]),
+    ];
+  }
 
-    /**
-     * Display the markup.
-     *
-     * @return array
-     *   Return markup array.
-     */
-    public function page2(){
-      return [
+  /**
+   * Display the markup.
+   *
+   * @return array
+   *   Return markup array.
+   */
+  public function page2() {
+    return [
       '#title' => 'Hi arijit this side',
-      '#data' => 'This is the data field'
-      ];
-    }
+      '#data' => 'This is the data field',
+    ];
+  }
 
 }
-
-?>
